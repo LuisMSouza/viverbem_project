c897719f689307f65dd3c49bd03c2374278e50b3
LuisMSouza <l.miguel-souza@hotmail.com>
2022-12-08 12:43:18 -0300

    commit update stage 2

--- src/main.c
+++ src/main.c
@@ -35,25 +35,52 @@ DataNasc Registro(char message[])
 
 void IniciarArquivos()
 {
-    if (Clientes = fopen('./data/clientes.txt', 'a+b') == NULL)
+    if (Clientes = fopen("./data/clientes.txt", "a+b") == NULL)
     {
-        printf("Arquivo de clientes criado com sucesso!");
+        printf("Criando arquivo de clientes...");
     }
-    if (Medicos = fopen('./data/medicos.txt', 'a+b') == NULL)
+    if (Medicos = fopen("./data/medicos.txt", "a+b") == NULL)
     {
-        printf("Arquivo de médicos criado com sucesso!");
+        printf("Criando arquivo de médicos...");
     }
-    if (Consultas = fopen('data/consultas.txt', 'a+b') == NULL)
+    if (Consultas = fopen("./data/consultas.txt", "a+b") == NULL)
     {
-        printf("Arquivo de consultas criado com sucesso!");
+        printf("Criando arquivo de consultas...");
     }
 }
 
-int main()
+int main(void)
 {
     setlocale(LC_ALL, "portuguese");
     IniciarArquivos(Clientes, Medicos, Consultas);
 
-    
+    int op;
+
+    printf("Sistema Viver Bem\n\nSeja Bem vindo!\n\nDigite o número "
+           "correspondente ao que deseja abaixo: \n\n1 - Cadastrar cliente\n2 - "
+           "Cadastrar consulta\n3- Cadastrar médico\n4 - Sair do sistema\n\n->");
+    scanf("%i", &op);
+    while (op > 4 || op < 1)
+    {
+        system("cls");
+        printf(
+            "Sistema Viver Bem\n\nSeja Bem vindo!\n\nDigite o número "
+            "correspondente ao que deseja abaixo: \n\n1 - Cadastrar cliente\n2 - "
+            "Cadastrar consulta\n3- Cadastrar médico\n4 - Sair do sistema\n\n-> ");
+        scanf("%i", &op);
+    }
+    switch (op)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        system("cls");
+        printf("Programa finalizado. Até breve!\n");
+        break;
+    }
     return 0;
 }
